b22411ffa1f0e07fe01e0ccae52fa2be60fc743c
renatasarmet <renatassmendes@hotmail.com>
2017-07-03 10:28:26 -0300

    Update transicao_horcrux2.hpp

--- transicao_horcrux2.hpp
+++ transicao_horcrux2.hpp
@@ -1,11 +1,3 @@
-//
-//  transicao_horcrux2.hpp
-//  HarryApp
-//
-//  Created by Renata Sarmet Smiderle Mendes on 22/06/17.
-//  Copyright © 2017 Renata Sarmet Smiderle Mendes. All rights reserved.
-//
-
 #ifndef TRANSICAO_HORCRUX2_HPP
 #define TRANSICAO_HORCRUX2_HPP
 
